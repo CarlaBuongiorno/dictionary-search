4dd45e36f6bc0d989b3924a60a87ff4a21e1543a
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-06-02 16:00:37 +0200

    Set up main.py and add find_key() function

--- dictionary_search.py
+++ dictionary_search.py
@@ -1,11 +1,2 @@
-'''
-Read the next exercise, but don't write the code.
-Write a suite of tests for it.
-
-Write a function that takes as input a dictionary, and a target string.
-The input dictionary may be nested (i.e. contain other dictionaries).
-Return True if any dictionary in the tree contains a key matching the target string.
-Call your module dictionary_search.py, and the function signature should be:
-
-def find_key(source_dictionary, target_string):
-   pass # should return boolean'''
+def find_key():
+    pass
