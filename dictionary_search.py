9be41b10762393c60679548196eeac0dde95efb7
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-06-05 11:45:01 +0200

    Add a function that returns True if any dictionary in a dictionary contains a key matching the target string

--- dictionary_search.py
+++ dictionary_search.py
@@ -1,2 +1,17 @@
-def find_key():
-    pass
+def find_key(source_dictionary, target_string):
+    match = False
+    if target_string in source_dictionary:
+        match = True
+    for key in source_dictionary:
+        if isinstance(source_dictionary[key], dict):
+            if target_string in source_dictionary[key]:
+                match = True
+            for nested_key in source_dictionary[key]:
+                if isinstance(source_dictionary[key][nested_key], dict):
+                    if target_string in source_dictionary[key][nested_key]:
+                        match = True
+                    for nested_nested_key in source_dictionary[key][nested_key]:
+                        if isinstance(source_dictionary[key][nested_key][nested_nested_key], dict):
+                            if target_string in source_dictionary[key][nested_key][nested_nested_key]:
+                                match = True
+    return match
--- test_dictionary_search.py
+++ test_dictionary_search.py
@@ -7,10 +7,26 @@ from dictionary_search import find_key
     ({},                            'key', False),
     ({'key2': 'value'},             'key', False),
     ({'key': 'value'},              'key', True),
+    ({'value': 'key'},              'key', False),
     ({'key2': {'key3': 'value'}},   'key', False),
     ({'key2': {'key': 'value'}},    'key', True),
+    ({'key1': {'key2': 'value'}, 'key2': {'alpha': 'sausage', 'key': 'value'}},    'key', True),
     ({'key2': {'key4': 'value'}, 'key3': {'key5': {'key6': 'value'}}}, 'key', False),
-    ({'key2': {'key4': 'value'}, 'key3': {'key5': {'key6': {'key8': 'value'}, 'key7': {'key': 'value'}}}}), 'key', True,
+    ({
+        'key2': {
+            'key4': 'value'
+        }, 
+        'key3': {
+            'key5': {
+                'key6': {
+                    'key8': 'value'
+                },
+                'key7': {
+                    'key': 'value'
+                }
+            }
+        }
+    },                              'key', True),
 ])
 def test_find_key(source_dictionary, target_string, expected):
     assert find_key(source_dictionary, target_string) == expected
