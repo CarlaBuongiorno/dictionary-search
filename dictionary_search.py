cfa3fb43e67cac7d1192d467e0c0db3cc26ee49b
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-06-10 10:25:18 +0200

    Add recursive functionality to search through a deeply nested dictionary for a target string

--- dictionary_search.py
+++ dictionary_search.py
@@ -1,17 +1,34 @@
 def find_key(source_dictionary, target_string):
-    match = False
     if target_string in source_dictionary:
-        match = True
+        return True
     for key in source_dictionary:
         if isinstance(source_dictionary[key], dict):
-            if target_string in source_dictionary[key]:
-                match = True
-            for nested_key in source_dictionary[key]:
-                if isinstance(source_dictionary[key][nested_key], dict):
-                    if target_string in source_dictionary[key][nested_key]:
-                        match = True
-                    for nested_nested_key in source_dictionary[key][nested_key]:
-                        if isinstance(source_dictionary[key][nested_key][nested_nested_key], dict):
-                            if target_string in source_dictionary[key][nested_key][nested_nested_key]:
-                                match = True
-    return match
+            if find_key(source_dictionary[key], target_string):
+                return True
+    return False
+
+
+
+
+
+# def find_key(source_dictionary, target_string):
+#     match = False
+#     if target_string in source_dictionary:
+#         match = True
+#     for key in source_dictionary:
+#         if isinstance(source_dictionary[key], dict):
+
+
+#             if target_string in source_dictionary[key]:
+#                 match = True
+#             for nested_key in source_dictionary[key]:
+#                 if isinstance(source_dictionary[key][nested_key], dict):
+#                     if target_string in source_dictionary[key][nested_key]:
+#                         match = True
+#                     for nested_nested_key in source_dictionary[key][nested_key]:
+#                         if isinstance(source_dictionary[key][nested_key][nested_nested_key], dict):
+#                             if target_string in source_dictionary[key][nested_key][nested_nested_key]:
+#                                 match = True
+
+
+#     return match
